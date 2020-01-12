dfc9ddf16ba28ce671a8d2d10cea7fb0a4071462
armannd092 <38623320+armannd092@users.noreply.github.com>
2020-01-12 12:30:15 +0100

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -17,32 +17,7 @@ def main():
     #filename=data[1]
     #data = data[0]
     DataMG.dataMG.exportCSV(data,filename)
-def experiment_01():
-    fp = r'CSV/201903-citibike-tripdata.csv'
-    obj = DataMG.dataMG(fp)
-    filename = DataMG.dataMG.exp_name(obj, freq='30D', n='Sample')
-    data = obj.read()
-    data =  DataMG.dataMG.sampler(data,frac=0.05)
-    #DataMG.dataMG.exportCSV(data, filename)
-    data = DataMG.dataMG.dir_tendency(data)
-    #print(data.head())
-def experiment_02():
-    fp = r'CSV/201903-citibike-tripdata.csv'
-    obj = DataMG.dataMG(fp)
-    data = obj.read()
-    dt = DataMG.dataMG.tripNum_time(data,saveName='TripNum_age5.pdf')
-    print(dt)
-    return None
-def experiment():
-    fp = r'CSV/201908-citibike-tripdata.csv'
-    obj = DataMG.dataMG(fp)
-    data = obj.read()
-    data_clean = DataMG.dataMG.clean(data)
-    #data_cls = DataMG.dataMG.kminClusterSt(data_clean,k=22)
-    #print(data_cls)
-    #ds = DataMG.dataMG.tripNum_St(data_clean,start=False)
-    dr = DataMG.dataMG.regression_plot(data_clean)
-    #DataMG.dataMG.exportCSV(data_cls,'st_cls_kmean.csv')
+    
 if __name__ == '__main__':
      main()
     
